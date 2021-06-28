a4a7085d0c963560d95ab1f25b3987039bca0043
Jonah Kagan <jonah@voting.works>
2021-06-28 15:08:06 -0700

    typecheck server

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from .app import app
 if __name__ == "__main__":
     app.run(
         use_reloader=FLASK_ENV in DEVELOPMENT_ENVS,
-        port=os.environ.get("PORT", 3001),
+        port=int(os.environ.get("PORT", 3001)),
         host="0.0.0.0",
         threaded=True,
     )
