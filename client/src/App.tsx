f7e3d473392fa9ba01009f2ce8ce90351a3f5ecf
Jonah Kagan <jonah@voting.works>
2021-06-29 13:08:19 -0700

    Basic react routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,59 @@
 import React from 'react'
-import { ApiProvider, useAdminUser } from './api'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ApiProvider, useAdminUser, AdminUser } from './api'
 
-const HomeScreen: React.FC = () => {
-  const userQuery = useAdminUser()
-  if (userQuery.isLoading || userQuery.isIdle) return null
-  const user = userQuery.isSuccess ? userQuery.data : null
-
+const AdminHome = ({ user }: { user: AdminUser }) => {
   return (
     <div>
       <header style={{ display: 'flex', justifyContent: 'space-between' }}>
         <a href="/" style={{ textDecoration: 'none', color: 'black' }}>
           Remote Ballot Marking by <strong>Voting</strong>Works
         </a>
-        {user ? (
-          <span>
-            {user.email} &bull; <a href="/auth/logout">Log out</a>
-          </span>
-        ) : (
-          <a href="/auth/login">Log in</a>
-        )}
+        <span>
+          {user.email} &bull; <a href="/auth/logout">Log out</a>
+        </span>
       </header>
-      {user && (
-        <div>
-          <h1>{user.organization.name}</h1>
-        </div>
-      )}
+      <div>
+        <h1>{user.organization.name}</h1>
+      </div>
     </div>
   )
 }
 
-const App: React.FC = () => (
+const LoginScreen = () => (
+  <div>
+    <h1>Remote Ballot Marking</h1>
+    <p>
+      by <strong>Voting</strong>Works
+    </p>
+    <a href="/auth/login">Log in</a>
+  </div>
+)
+
+const Routes = () => {
+  const userQuery = useAdminUser()
+  if (userQuery.isLoading || userQuery.isIdle) return null
+  const user = userQuery.isSuccess ? userQuery.data : null
+
+  if (user === null) return <LoginScreen />
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/ballot">
+          <p>Ballot component goes here</p>
+        </Route>
+        <Route path="/">
+          <AdminHome user={user} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+const App = () => (
   <ApiProvider>
-    <HomeScreen />
+    <Routes />
   </ApiProvider>
 )
 
